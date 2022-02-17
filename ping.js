fb62a6a9a98741a2bbf9064f956345cb7060f3c1
Constança Rigueira <83832283+conyrig@users.noreply.github.com>
2022-02-17 00:19:13 +0000

    Update ping.js

--- ping.js
+++ ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
+        /*Send "pong!" message to channel when user inserts "ping"*/
         message.channel.send('pong!');
     }
 }
